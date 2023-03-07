f681134a48d81a887bbc1ce2b874d2664521812a
Gunsnfnr <a.j.p.010203@gmail.com>
2023-03-07 17:03:49 +0300

    added Layout in Main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,5 +1,10 @@
+import { Layout } from '../Layout/Layout.jsx';
 import style from './Main.module.css';
 
 export const Main = () => {
-  return <div className={style.main}></div>
+  return (
+    <Layout>
+      <div className={style.main}></div>
+    </Layout>
+  )
 }
