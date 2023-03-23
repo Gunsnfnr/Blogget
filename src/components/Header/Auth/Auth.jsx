67adf54d53f1323c471f238280c1815795e93f8c
Gunsnfnr <a.j.p.010203@gmail.com>
2023-03-23 18:54:25 +0300

    lesson 6

--- src/components/Header/Auth/Auth.jsx
+++ src/components/Header/Auth/Auth.jsx
@@ -4,7 +4,7 @@ import style from './Auth.module.css';
 import {ReactComponent as LoginIcon} from './img/login.svg';
 import {urlAuth} from '../../../api/auth';
 import {Text} from '../../../UI/Text/Text';
-import {useAuth} from '../../../hooks/useAuthhook.js';
+import {useAuth} from '../../../hooks/useAuthHook.js';
 
 export const Auth = ({token, delToken}) => {
   const [auth, setAuth] = useState({});
